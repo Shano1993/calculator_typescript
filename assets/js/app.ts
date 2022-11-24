742cd7e2ac3fca1fd41b828c3742205132930e23
Shano1993 <stefan.hanotiau@gmail.com>
2022-11-24 13:58:33 +0100

    calculator

--- assets/js/app.ts
+++ assets/js/app.ts
@@ -5,25 +5,22 @@ let buttonNumber: HTMLCollectionOf<HTMLButtonElement> = document.getElementsByCl
 let operation: HTMLCollectionOf<HTMLButtonElement> = document.getElementsByClassName("operation") as HTMLCollectionOf<HTMLButtonElement>;
 let resultButton: HTMLButtonElement = document.getElementById("result_button") as HTMLButtonElement;
 
-for (let button of buttonNumber){
-    if (button) {
-        button.addEventListener("click", function (): string {
-            let dataNumber: string = button.innerHTML;
-            return result.innerHTML += dataNumber;
+function valueButtons(collection: HTMLCollectionOf<HTMLButtonElement>) {
+    for (let button of collection){
+        if (button) {
+            button.addEventListener("click", function (): string {
+                let data: string = button.innerHTML;
+                return result.innerHTML += data;
+            })
+        }
+        resultButton.addEventListener("click", function () {
+
         })
     }
 }
 
-for (let button of operation) {
-    if (button) {
-        button.addEventListener("click", function (): string {
-            let data: string = button.innerHTML;
-            return result.innerHTML += data;
-        })
-    }
-}
+valueButtons(buttonNumber);
+valueButtons(operation);
 
-resultButton.addEventListener("click", function (): string {
-    return result.innerHTML = eval(result.innerHTML);
-})
+
 
