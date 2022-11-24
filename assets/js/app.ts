fcbd89535565fa6f690d859b029261bfe66b1521
Shano1993 <stefan.hanotiau@gmail.com>
2022-11-24 12:08:16 +0100

    calculator

---
+++ assets/js/app.ts
@@ -0,0 +1,29 @@
+import "../styles/style.scss";
+
+let result: HTMLSpanElement = document.getElementById("result") as HTMLSpanElement;
+let buttonNumber: HTMLCollectionOf<HTMLButtonElement> = document.getElementsByClassName("button_number") as HTMLCollectionOf<HTMLButtonElement>;
+let operation: HTMLCollectionOf<HTMLButtonElement> = document.getElementsByClassName("operation") as HTMLCollectionOf<HTMLButtonElement>;
+let resultButton: HTMLButtonElement = document.getElementById("result_button") as HTMLButtonElement;
+
+for (let button of buttonNumber){
+    if (button) {
+        button.addEventListener("click", function (): string {
+            let dataNumber: string = button.innerHTML;
+            return result.innerHTML += dataNumber;
+        })
+    }
+}
+
+for (let button of operation) {
+    if (button) {
+        button.addEventListener("click", function (): string {
+            let dataOperande: string = button.innerHTML;
+            return result.innerHTML += dataOperande;
+        })
+    }
+}
+
+resultButton.addEventListener("click", function (): string {
+    return result.innerHTML = eval(result.innerHTML);
+})
+
